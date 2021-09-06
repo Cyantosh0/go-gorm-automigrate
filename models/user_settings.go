98ce649f909f2c13e9e8e5c44e4296c77b7ea290
Cyantosh0 <shresthasantosh799@gmail.com>
2021-09-06 17:12:19 +0545

    Fix:Update 'value' size to '200' in 'user_settings' table

--- models/user_settings.go
+++ models/user_settings.go
@@ -5,7 +5,7 @@ import "time"
 type UserSettings struct {
 	UserID    string    `gorm:"unique_index;size:28;not null" json:"userId"`
 	Name      string    `gorm:"unique_index;size:45;not null" json:"name"`
-	Value     string    `gorm:"size:255;not null" json:"value"`
+	Value     string    `gorm:"size:200;not null" json:"value"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"createdAt"`
 	UpdatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP" json:"updatedAt"`
 }
